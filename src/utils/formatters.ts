b68f1eec6210f3ae693a1242fff8f5b750b8ebad
balazs.takacs <balazs.takacs@aldi-sued.com>
2026-03-07 19:19:45 +0100

    Refactor formatter

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,16 +1,13 @@
 export function formatAccountNumber(accountNumber: string) {
   const digits = accountNumber.replace(/\D/g, "");
 
-  let formattedAccountNumber = "";
-
   if (digits.length <= 3) {
-    formattedAccountNumber = digits;
+    return digits;
   } else if (digits.length <= 10) {
-    formattedAccountNumber = digits.slice(0, 3) + "-" + digits.slice(3);
+    return digits.slice(0, 3) + "-" + digits.slice(3);
   } else {
-    formattedAccountNumber =
-      digits.slice(0, 3) + "-" + digits.slice(3, 10) + "-" + digits.slice(10);
+    return (
+      digits.slice(0, 3) + "-" + digits.slice(3, 10) + "-" + digits.slice(10)
+    );
   }
-
-  return formattedAccountNumber;
 }
